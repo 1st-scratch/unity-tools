c5fab184e36b7d7881cbe2f2fe51969cc09705ba
mmyano <m.yano@dropseed.co.jp>
2021-09-03 18:50:52 +0900

    Rename files

--- unity-version.sh
+++ unity-version.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-OUTPUT_FILE="versions.txt"
+OUTPUT_FILE="unity-versions.txt"
 touch $(pwd)/$OUTPUT_FILE
 /Applications/Unity\ Hub.app/Contents/MacOS/Unity\ Hub -- --headless editors --installed | cut -f 1 -d "," > $OUTPUT_FILE
 echo "---- version info ----"
